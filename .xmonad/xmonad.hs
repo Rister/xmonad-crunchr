27ffebfd3cb6aab41347a356d72322eae2eedd0a
Rister <rist.jeremy@gmail.com>
2020-11-10 09:13:00 -0500

    tray size

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -119,7 +119,7 @@ myManageHook =
 
 myStartupHook = do
   -- spawnOnce "nitrogen --restore &" -- set the desktop background
-  spawnOnce "trayer --edge top --align right --widthtype percent --width 11 --margin 0 --padding 6 --SetDockType true --SetPartialStrut true --expand false --monitor primary --transparent true --alpha 1 --tint 0x000000 --height 24 &" -- system tray
+  spawnOnce "trayer --edge top --align right --widthtype percent --width 7 --margin 0 --padding 6 --SetDockType true --SetPartialStrut true --expand false --monitor primary --transparent true --alpha 1 --tint 0x000000 --height 24 &" -- system tray
   spawnOnce "nm-applet &" -- network manager icon
   spawnOnce "volumeicon &" -- Volume tray icon
   spawnOnce "xscreensaver -no-splash &" -- Start the screensaver daemon
